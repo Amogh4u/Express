bfa7b05d24a3e9ca5b70f057ef676b8d1595b7a9
Amogh <amogh.r@servify.in>
2020-12-16 17:27:08 +0530

    added app listen for heruku

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.use('/options', optionsRouter);
 app.get('/', (req,res) => {
     res.render('index')
 })
-app.listen(3000, () => {
-    console.log('Server Started');
-});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+// app.listen(3000, () => {
+//     console.log('Server Started');
+// });
